5428012f9dc56146d991cf91a1565ab33aaf73fc
Abhinil-07 <abhinilnath10@gmail.com>
2024-05-14 00:46:23 +0530

    Integrated python backend

--- components/component/dashboard.tsx
+++ components/component/dashboard.tsx
@@ -3,8 +3,46 @@ import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import axios from "axios";
 
 export function MyDashboard() {
+  const [imageFile, setImageFile] = useState("");
+
+  const handleChange = (e: any) => {
+    setImageFile(e.target.files[0]);
+    console.log(e.target.files[0]);
+  };
+  const handleClick = async () => {
+    try {
+      console.log("rendi rajit");
+      const formData = new FormData();
+      formData.append("image", imageFile);
+      formData.append(
+        "jsonData",
+        JSON.stringify({
+          classroomId: "6640fbd74ca657ece9b9d5ea",
+          studentId: "6642594aee1e7d26fa1fe392",
+        })
+      ); // Convert JSON object to string
+
+      const response = await axios.post(
+        "http://127.0.0.1:5000/image/individual",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      console.log("Upload successful:", response.data);
+      alert("Upload successful!");
+    } catch (error) {
+      console.error("Error uploading:", error);
+      alert("Upload failed. Please try again.");
+    }
+  };
   return (
     <>
       <main className="flex-1 py-10 px-4 md:px-6">
@@ -12,9 +50,11 @@ export function MyDashboard() {
           <h1 className="text-2xl font-bold">Upload Image</h1>
           <div className="grid w-full max-w-sm items-center gap-1.5">
             <Label htmlFor="picture">Picture</Label>
-            <Input id="picture" type="file" />
+            <Input onChange={handleChange} id="picture" type="file" />
           </div>
-          <Button className="w-full">Upload</Button>
+          <Button onClick={handleClick} className="w-full">
+            Upload
+          </Button>
         </div>
       </main>
     </>
